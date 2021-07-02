2fc5e4ab1a730210982e9891cb1dae1a305259f7
Stanley Piotrowski <stanleyjpiotrowski@gmail.com>
2021-07-01 22:22:35 -0700

    Reformatted code for legibility

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
-# Purpose: Say hello
+"""
+Author: Stanley Piotrowski <stanley.piotrowski@gmail.com>
+Purpose: Say hello
+"""
 
 import argparse
 
-parser = argparse.ArgumentParser(description = "Say hello")
-parser.add_argument("-n", "--name", metavar = "name", default = "World", help = "Name to greet")
-args = parser.parse_args()
+# -----------------------------------------------------------------------
+def get_args():
+    """Get the command-line arguments"""
 
-print("Hello, " + args.name + "!")
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument("-n", "--name", metavar="name",
+                        default="World", help="Name to greet")
+
+    return parser.parse_args()
+
+# -------------------------------------------------------------------------
+def main():
+    """Define the main function"""
+
+    args = get_args() # Call this function to get the parsed arguments
+    print("Hello, " + args.name + "!")
+
+# ---------------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
